1a8fbd0535d3da97359efdec36160da59e945bd5
zxfan <zifanxu96@gmail.com>
2020-09-26 21:27:12 -0500

    added container script

--- docker_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-HASH=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
-name=${USER}_APPLR_${HASH}
-
-echo "Launching container named '${name}'"
-# Launches a docker container using our image, and runs the provided command
-
-singularity exec -i -n -p -B `pwd`:/APPLR APPLR_melodic.simg /bin/bash -c 'source /jackal_ws/devel/setup.bash; python3 ${@:1}'
---
+++ entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /jackal_ws/devel/setup.bash
+cd /APPLR/discrete
+exec ${@:1}
