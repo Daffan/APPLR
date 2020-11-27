eb2073dfb5df730568a4c18aa0aa94bb41deefcf
Zifan Xu <zifan@cs.utexas.edu>
2020-11-27 10:17:28 -0600

    clean useless code

--- continuous/local_actor.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PATH=$PATH:/lusr/opt/singularity-3.2.1/bin
-export ROS_LOG_DIR=/tmp
-singularity exec -i --nv -n --network=none -p -B `pwd`:/APPLR APPLR_melodic.simg /bin/bash /APPLR/entrypoint.sh python3 actor.py --id ${@:1}
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 source /jackal_ws/devel/setup.bash
-cd /APPLR/continuous
-#cd /APPLR/discrete
+cd /APPLR
 exec ${@:1}
